06b65c01e26bdaf14746fc9c08f070f4851d5b2f
qinchong <qinchong@iliangcang.com>
2017-08-15 18:49:34 +0800

    fix

--- signal.c
+++ signal.c
@@ -3,6 +3,16 @@
 #include <unistd.h>
 void sig_hand(int);
 void sig_hand2(int);
+
+/**
+ * @file signal.c
+ * @brief 本例可以看出如果不用换行符刷新缓冲区，输出会混乱
+ * 标准的io函数都是不可重入的
+ * @author Bruce
+ * @version 
+ * @date 2017-08-15
+ */
+
 int main()
 {
 	if(signal(SIGUSR1, sig_hand) == SIG_ERR)
@@ -24,9 +34,9 @@ void sig_hand(int signo)
 			if(i == 20)
 				break;
 			sleep(1);
-			printf("i=%d\n", i);
+			printf(",i=%d", i);
 		}
-		printf("%d\n", i);
+		printf("end:%d\n", i);
 		char buf[] = "sigusr1\n";
 		write(STDOUT_FILENO, buf, strlen(buf));
 	}
@@ -41,6 +51,16 @@ void sig_hand2(int signo)
 {
 	if(signo == SIGUSR2)
 	{
+		int i=0;
+		while(1)
+		{
+			i++;
+			if(i == 20)
+				break;
+			sleep(1);
+			printf("%d->", i);
+		}
+		printf("end2:%d\n", i);
 		char buf[] = "sigusr2\n";
 		write(STDOUT_FILENO, buf, strlen(buf));
 	}
