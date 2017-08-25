e140cf9aa9809bad9b0a98630fda0f0b332c21a1
qinchong <qinchong@iliangcang.com>
2017-08-25 14:15:17 +0800

    fix

--- readv.c
+++ readv.c
@@ -4,27 +4,36 @@
 
 int main()
 {
-	char buf1[5],buf2[10];
+	char buf1[10],buf2[10];
 	memset(buf1, 0, sizeof(buf1));
 	memset(buf2, 0 ,sizeof(buf2));
 	struct iovec iov[2];
-	iov[0].iov_base = buf1;
-	iov[0].iov_len = 5;
-	iov[1].iov_base = buf2;
-	iov[1].iov_len = 10;
-
 	int fd = open("/tmp/a.txt", O_RDWR);
 	if( fd < 0 )
 		err_sys("open error");
-	int rsize = readv(fd, iov, 2);
+	int rsize = read(fd, buf1, 10);
 	if( rsize < 0 )
 		err_sys("readv error");
 	printf("read size:%d\n", rsize);
+	buf1[rsize] = '\0';
+	printf("buf1:%s", buf1);
 	close(fd);
-	fd = open("/tmp/b.txt", O_RDWR|O_CREAT);
+	fd = open("/tmp/c.txt", O_RDWR);
+	if( fd < 0 )
+		err_sys("open error");
+	int rsize2 = read(fd, buf2, 10);
+	if( rsize < 0 )
+		err_sys("readv error");
+	printf("read size:%d\n", rsize2);
+	close(fd);
+	iov[0].iov_base = buf1;//每个缓冲区的长度可以按照读入的长度确定
+	iov[0].iov_len = rsize;
+	iov[1].iov_base = buf2;//多册读入，离散读
+	iov[1].iov_len = rsize2;
+	fd = open("/tmp/b.txt", O_RDWR|O_CREAT, 0644);
 	if(fd < 0)
 		err_sys("open error");
-	int wsize = writev(fd, iov, 2);
+	int wsize = writev(fd, iov, 2);//一次性写入，聚集写
 	if(wsize < 0 )
 		err_sys("writev error");
 	printf("write size:%d\n", wsize);
